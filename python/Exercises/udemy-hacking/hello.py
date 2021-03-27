2396352f0daffb6729006b4b3cd84e490de251d3
nyalaman <nyalaclub@gmail.com>
2021-03-27 23:38:31 +0200

    Just resolving a few things

---
+++ python/Exercises/udemy-hacking/hello.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 27 17:04:59 2021
+
+@author: julian
+"""
+
+print("Hello world!")
+print("\n")
+print('Hello World!')
+
+print("""This string runs 
+on multiple lines""")
+
+print("This string is " + "awesome")
---
+++ python/Exercises/udemy-hacking/math.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 27 17:12:59 2021
+
+@author: julian
+"""
+
+print(50+50)
+print(50-50)
+print(50*50)
+print(50/50)
+print(50 + 50 - 50 * 50 / 50)
